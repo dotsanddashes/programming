f5716807752f30e8c6e51ea7f474152f76e4e04e
Студент НИУ ВШЭ <student@basm-402-std11.staff.corp.local>
2017-09-19 11:58:47 +0300

    dfhl;
    Signed-off-by: Студент НИУ ВШЭ <student@basm-402-std11.staff.corp.local>

---
+++ first_class.py
@@ -0,0 +1,20 @@
+print("Hello!")
+
+#x = 10
+#y = x*100    # умножаю на 100
+
+#print('y =',y)
+#print('x =', x)
+
+#human_name = input('Как вас зовут? ')
+#print('Привет,', human_name)
+#print(type(human_name))
+
+one = float(input('Введите первое число: '))
+#two = float(input('Введите второе число: '))
+#mult = one*two
+#print('Результат:', mult)
+
+if one > 5:
+    print('Ваше число больше пяти')
+    print('!!')
