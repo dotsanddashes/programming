36582180371f2aa1cf072463d35be744a7a9f623
dotsanddashes <nemirw@gmail.com>
2017-10-02 19:41:52 +0300

    домашнее задание по программированию

---
+++ hw1/homework1_var5.py
@@ -0,0 +1,19 @@
+input_a = input('Введите число а: ')
+input_b = input('Введите число b: ')
+input_c = input('Введите число c: ')
+
+a = float(input_a)
+b = float(input_b)
+c = float(input_c)
+
+if a*b == c:
+    print('Произведение a и b дает c')
+else:
+    print('Произведение a и b НЕ дает c')
+
+if a/b == c:
+    print('Если a разделить на b, получится c')
+else:
+    print('Если a разделить на b, c НЕ получится :(')
+
+print('End.')
