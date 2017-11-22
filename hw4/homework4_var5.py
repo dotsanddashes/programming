915973d86c19f7ddaae2d0fe767147ae0d9faa39
dotsanddashes <nemirw@gmail.com>
2017-11-22 17:52:21 +0300

    homework4

---
+++ hw4/homework4_var5.py
@@ -0,0 +1,10 @@
+with open("text.txt", encoding="utf-8") as f:
+    text = f.read()
+    splited_text = text.split()
+all_words = len(splited_text)
+a = 0
+for word in splited_text:
+    if len(word) >= 10:
+        a += 1
+print("Доля длинных слов: ", a / all_words)
+print("End.")
