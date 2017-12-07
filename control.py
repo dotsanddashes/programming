6e88731f15cdff095fe60aed220f1638969d3c19
Vasilisa Kutuzova <31654757+dotsanddashes@users.noreply.github.com>
2017-12-07 11:36:13 +0300

    контрольная работа

---
+++ control.py
@@ -0,0 +1,15 @@
+part = []
+sum_ipm = 0
+with open ('freq.txt', 'r', encoding = 'utf-8') as f:
+    lines = f.readlines()
+    for line in lines:
+        line_list = line.split()
+        #if len(line_list) >= 5:
+            #if line_list[4] == 'перех':
+                #print(line)
+        if line_list[2] == 'част':
+            part.append(line_list[0])
+            sum_ipm = sum_ipm + float(line_list[-1])
+print(part)
+print('Сумма ipm: ', sum_ipm)
+            
