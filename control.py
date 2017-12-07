ddb71718e43b8b8216d63dad978ec7cc2c81ecff
Vasilisa Kutuzova <31654757+dotsanddashes@users.noreply.github.com>
2017-12-07 11:36:43 +0300

    Update control.py

--- control.py
+++ control.py
@@ -4,9 +4,9 @@ with open ('freq.txt', 'r', encoding = 'utf-8') as f:
     lines = f.readlines()
     for line in lines:
         line_list = line.split()
-        #if len(line_list) >= 5:
-            #if line_list[4] == 'перех':
-                #print(line)
+        if len(line_list) >= 5:
+            if line_list[4] == 'перех':
+                print(line)
         if line_list[2] == 'част':
             part.append(line_list[0])
             sum_ipm = sum_ipm + float(line_list[-1])
