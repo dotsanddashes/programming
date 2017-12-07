708d3c1f075861041af65837a6578c1a37256c65
Vasilisa Kutuzova <31654757+dotsanddashes@users.noreply.github.com>
2017-12-07 12:08:18 +0300

    контрольная

---
+++ test2/control.py
@@ -0,0 +1,28 @@
+part = []
+sum_ipm = 0
+word_list = []
+with open ('freq.txt', 'r', encoding = 'utf-8') as f:
+    lines = f.readlines()
+    #for line in lines:
+       #line_list = line.split()
+       #if len(line_list) >= 5:
+            #if line_list[4] == 'перех':
+                #print(line)
+    #for line in lines:
+     #   line_list = line.split()
+      #  if line_list[2] == 'част':
+       #     part.append(line_list[0])
+        #    sum_ipm = sum_ipm + float(line_list[-1])
+#print(part)
+#print('Сумма ipm: ', sum_ipm)
+
+word = input('Введите слово: ')
+if word == '':
+    print('Вы не ввели слово.')
+else:
+    while word != '':
+        for line in lines:
+            line_list = line.split()
+            if line_list[0] == word:
+                print(line_list[:2])
+            word = input()
