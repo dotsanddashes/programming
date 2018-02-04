fb5a43bbec4c370d012e2ada96d2ebe6990de69c
dotsanddashes <nemirw@gmail.com>
2018-02-04 20:06:29 +0300

    исправления

--- hw7/homework7_var5.py
+++ hw7/homework7_var5.py
@@ -1,30 +1,39 @@
-def open_file(fname): #открывает файл и возвращает список слов
+def file_name(): #проверяет существование файла
+    fname = input('Введите имя файла: ')
+    try:
+        open(fname)
+        return(fname)
+    except FileNotFoundError:
+        return print('Файл не найден. Попробуйте еще раз.') + exit(0)
+def open_file(): #открывает файл и возвращает список слов
+    fname = file_name()
     with open(fname, encoding = 'utf-8') as f:
-        text = f.read()
-        splited_text = text.split()
+            text = f.read()
+            splited_text = text.split()
     return splited_text
-def words_search(): #ищет прилагательные на -ous и заносит их в словарь следующим образом: прилагательное - ключ, его длина - значение
-    words = open_file('Pride_and_Prejudice.txt')
+def words_search(): #ищет прилагательные на -ous и создает список
+    words = open_file()
+    adj_list = []
+    for word in words:
+        if word.endswith('ous'):
+            adj_list.append(word)
+    return adj_list
+def how_many(l): #cчитает количество прилагательных
+    return print('Всего прилагательных, кончающихся на -ous: ', len(l))
+def mid_length(a): #создает словарь и считает среднюю длину прилагательного
+    words = a
     dictionary = {}
     for word in words:
-        if word.endswith('ous'):
-            dictionary[word] = len(word)
-    return dictionary
-def how_many(): #считает количество прилагательных на -ous
-    words = open_file('Pride_and_Prejudice.txt')
-    i = 0
-    for word in words:
-        if word.endswith('ous'):
-            i += 1
-    return print('Всего прилагательных, заканчивающихся на -ous: ', i)
-def mid_length(): #считает среднюю длину
-    dictionary = words_search()
+        dictionary[word] = len(word)
     i = 0
     a = 0
     for num in dictionary.values():
         a += 1
         i += int(num)
     return print('Средняя длина прилагательного: ', i/a)
-
-how_many()
-mid_length()
+def main():
+    my_var = words_search()
+    how_many(my_var)
+    mid_length(my_var)
+if __name__ == '__main__':
+    main()
